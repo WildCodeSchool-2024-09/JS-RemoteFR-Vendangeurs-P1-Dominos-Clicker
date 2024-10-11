3feec0e3d21291bba8c4f5887fa42f7299383d2f
Leslaures <lauremaffini@gmail.com>
2024-10-11 10:32:45 +0200

    [feat] : travail sur le menu cliquable

--- script.js
+++ script.js
@@ -13,112 +13,107 @@ let anchoy = 0
 let compteur = 0
 
 let images = [
-  "./public/pizza.png", //Image par default
-  "./public/pizza1.png",
-  "./public/pizza2.png",
-  "./public/pizza3.png",
-  "./public/pizza4.png",
-  "./public/pizza5.png",
-  "./public/pizza6.png",
-  "./public/pizza7.png"
+"./public/pizza.png", //Image par default
+"./public/pizza1.png",
+"./public/pizza2.png",
+"./public/pizza3.png",
+"./public/pizza4.png",
+"./public/pizza5.png",
+"./public/pizza6.png",
+"./public/pizza7.png"
 ];
 
 
-function addPizzaSlice(){pizzas = pizzas+1;
-  document.getElementById("sliceOfPizza_count").innerHTML = parseInt(pizzas);
-  
-  
+function addPizzaSlice(){
+pizzas = pizzas+1;
+document.getElementById("sliceOfPizza_count").innerHTML = parseInt(pizzas);
 }
-function ShroomAddPizzaSlice(){pizzas = pizzas + (shrooms * pizzasMultiple);
+
+function ShroomAddPizzaSlice(){
+pizzas = pizzas + (shrooms * pizzasMultiple);
+document.getElementById("sliceOfPizza_count").innerHTML = parseInt(pizzas);
+}
+
+function addShroom(){
+shrooms = shrooms +1;
+
+setInterval(function(){
+    ShroomAddPizzaSlice()
+}, 1000)
+
+document.getElementById("shroomCount").innerHTML = shrooms;
+}
+
+function tomatosAddPizzaSlice(){pizzas = pizzas + (tomatos * pizzasMultiple);
   document.getElementById("sliceOfPizza_count").innerHTML = parseInt(pizzas);
 
 
 }
-function addShroom(){shrooms = shrooms +1;
- 
+
+function addTomatos(){tomatos = tomatos +1;
+
+
     setInterval(function(){
-      ShroomAddPizzaSlice()
+      tomatosAddPizzaSlice()
   
   
-     }, 1000)
-     document.getElementById("shroomCount").innerHTML = shrooms;
+      }, 1000)
+      document.getElementById("tomatosCount").innerHTML = tomatos;
     }
     
   
+  
+    ////////////////////////
+    
 
- 
-
-
-   function tomatosAddPizzaSlice(){pizzas = pizzas + (tomatos * pizzasMultiple);
+    function cheeseAddPizzaSlice(){pizzas = pizzas + (cheese * pizzasMultiple);
     document.getElementById("sliceOfPizza_count").innerHTML = parseInt(pizzas);
   
   
   }
 
-  function addTomatos(){tomatos = tomatos +1;
-
-
+  function addCheese(){cheese = cheese +1;
+    
       setInterval(function(){
-        tomatosAddPizzaSlice()
+        cheeseAddPizzaSlice()
     
     
-       }, 1000)
-       document.getElementById("tomatosCount").innerHTML = tomatos;
+        }, 1000)
+        document.getElementById("cheeseCount").innerHTML = cheese;
       }
       
     
-   
-     ////////////////////////
-     
 
-     function cheeseAddPizzaSlice(){pizzas = pizzas + (cheese * pizzasMultiple);
+
+      ///////
+
+      function anchoyAddPizzaSlice(){pizzas = pizzas + (anchoy * pizzasMultiple);
       document.getElementById("sliceOfPizza_count").innerHTML = parseInt(pizzas);
     
     
     }
   
-    function addCheese(){cheese = cheese +1;
-      
-        setInterval(function(){
-          cheeseAddPizzaSlice()
-      
-      
-         }, 1000)
-         document.getElementById("cheeseCount").innerHTML = cheese;
-        }
-       
-      
-
-
-       ///////
-
-       function anchoyAddPizzaSlice(){pizzas = pizzas + (anchoy * pizzasMultiple);
-        document.getElementById("sliceOfPizza_count").innerHTML = parseInt(pizzas);
-      
-      
-      }
+    function addAnchoy(){anchoy = anchoy +1;
+      setInterval(function(){
+        anchoyAddPizzaSlice()
     
-      function addAnchoy(){anchoy = anchoy +1;
-        setInterval(function(){
-          anchoyAddPizzaSlice()
-      
-      
-         }, 1000)
-         document.getElementById("anchoyCount").innerHTML = anchoy;}
-     
+    
+        }, 1000)
+        document.getElementById("anchoyCount").innerHTML = anchoy;}
+    
 
 
-     ////////////////////////
-      function pizzaClick(){
+    ////////////////////////
+    function pizzaClick(){
 
-        compteur= compteur +1;
-        if(compteur >= images.length){
-          compteur = 0;
-        } 
-        let image = document.getElementById ("pizza_img");
-        image.src=images[compteur];
+      compteur= compteur +1;
+      if(compteur >= images.length){
+        compteur = 0;
+      } 
+      let image = document.getElementById ("pizza_img");
+      image.src=images[compteur];
 
-         }
+        }
 
 ////////////////////////
 // FENETRE MODAL ONCLICK
@@ -130,13 +125,13 @@ const closeModalPizzaBtn = document.querySelector("#closeModalPizzaBtn");
 
 
 openModalPizzaBtn.addEventListener("click" , () => {
-  modalPizza.className = "modal";
+modalPizza.className = "modal";
 })
 
 closeModalPizzaBtn.addEventListener("click" , () => {
-  console.log("toto");
-  modalPizza.className = "modalHidden";
+console.log("toto");
+modalPizza.className = "modalHidden";
 })
 
 
-   
+  
